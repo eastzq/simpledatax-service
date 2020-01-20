7650c74713f6af7e7f5ec08874d1c6824c78b93c
eastzq <eastzq@gmail.com>
2020-01-20 09:34:52 +0800

    update

--- src/main/java/com/github/simpledatax/plugin/PluginResouce.java
+++ src/main/java/com/github/simpledatax/plugin/PluginResouce.java
@@ -1,51 +1,56 @@
 package com.github.simpledatax.plugin;
 
 public enum PluginResouce {
-    RDBMS_READER("reader","rdbmsreader", "com.github.simpledatax.plugin.reader.rdbmsreader.RdbmsReader",""),
-	MYSQL_READER("reader","mysqlreader", "com.github.simpledatax.plugin.reader.mysql.MySqlReader",""),   
-    MYSQL_WRITER("writer", "mysqlwriter", "com.github.simpledatax.plugin.writer.mysqlwriter.MysqlWriter",""),
-	RDBMS_WRITER("writer","rdbmswriter", "com.github.simpledatax.plugin.writer.rdbmswriter.RdbmsWriter","");
-    
-    private PluginResouce(String type, String name, String className,String desc) {
+    RDBMS_READER("reader", "rdbmsreader", "com.github.simpledatax.plugin.reader.rdbmsreader.RdbmsReader", ""),
+    RDBMS_WRITER("writer", "rdbmswriter", "com.github.simpledatax.plugin.writer.rdbmswriter.RdbmsWriter", ""),
+
+    ORACLE_READER("reader", "oraclereader", "com.github.simpledatax.plugin.reader.oraclereader.OracleReader", ""),
+    ORACLE_WRITER("writer", "oraclewriter", "com.github.simpledatax.plugin.writer.oraclewriter.OracleWriter", ""),
+
+    MYSQL_READER("reader", "mysqlreader", "com.github.simpledatax.plugin.reader.mysql.MySqlReader", ""),
+    MYSQL_WRITER("writer", "mysqlwriter", "com.github.simpledatax.plugin.writer.mysqlwriter.MysqlWriter", "");
+
+    private PluginResouce(String type, String name, String className, String desc) {
         this.type = type;
         this.name = name;
         this.className = className;
         this.desc = desc;
     }
-	private String type;
-	private String name;
-	private String desc;
-	private String className;
 
-	public String getName() {
-		return name;
-	}
+    private String type;
+    private String name;
+    private String desc;
+    private String className;
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getDesc() {
-		return desc;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setDesc(String desc) {
-		this.desc = desc;
-	}
+    public String getDesc() {
+        return desc;
+    }
 
-	public String getClassName() {
-		return className;
-	}
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
 
-	public void setClassName(String className) {
-		this.className = className;
-	}
+    public String getClassName() {
+        return className;
+    }
 
-	public String getType() {
-		return type;
-	}
+    public void setClassName(String className) {
+        this.className = className;
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
